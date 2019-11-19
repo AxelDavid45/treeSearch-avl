1bd77193e180e47a3712cf804316789f41a5b4c1
Axel Espinosa <axeldavid45@gmail.com>
2019-11-19 10:38:33 -0600

    Antes agregar altura

--- arboles/Implementacionarbol.java
+++ arboles/Implementacionarbol.java
@@ -3,24 +3,24 @@ package arboles;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-
 public class Implementacionarbol {
+
     public static void main(String[] args) {
         try {
-            arbolbuscador myTree= new arbolbuscador();
-                        
+            arbolbuscador myTree = new arbolbuscador();
+
             myTree.insertar(new nodo(8));
             myTree.insertar(new nodo(3));
             myTree.insertar(new nodo(1));
             myTree.insertar(new nodo(20));
             myTree.insertar(new nodo(10));
             myTree.insertar(new nodo(5));
-            
+
             myTree.print(myTree.getRoot());
             System.out.println("-----------");
             myTree.eliminar(new nodo(10));
             myTree.print(myTree.getRoot());
-         
+
 //         if(myTree.delete(myTree.getRoot(), new nodo(10)) ==null)
 //             System.out.println("Nodo eliminado");
 //         else
@@ -30,7 +30,5 @@ public class Implementacionarbol {
             Logger.getLogger(Implementacionarbol.class.getName()).log(Level.SEVERE, null, ex);
         }
 
-       
-}
     }
-   
+}
